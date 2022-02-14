2d3608e118c35746bbe7cfe6c9a88772e008b223
Vincent Chan <okcdz@diverse.space>
2022-02-14 10:37:17 +0800

    write docs

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/vincentdchan/LichenScript',
+  docsRepositoryBase: 'https://github.com/vincentdchan/lichenscript-docs',
+  titleSuffix: ' – LichenScript',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">LichenScript</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        A lightweight language compiled to JavaScript/C.
       </span>
     </>
   ),
@@ -16,14 +16,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="description" content="LichenScript: A lightweight language compiled to JavaScript/C." />
+      <meta name="og:description" content="A lightweight language compiled to JavaScript/C." />
+      {/* <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="twitter:url" content="https://nextra.vercel.app" /> */}
+      <meta name="og:title" content="A lightweight language compiled to JavaScript/C." />
+      {/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
